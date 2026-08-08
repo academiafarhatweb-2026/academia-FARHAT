f6da0a57cb90b864d96a636800459334352feb92
Marisol Lamas <marilamas02@gmail.com>
2026-08-08 11:12:03 -0300

    feat: agrega validaciones a las rutas de estudiantes y profesores para mejorar la gestión de datos

--- server/src/routes/studentRoutes.js
+++ server/src/routes/studentRoutes.js
@@ -2,14 +2,15 @@ const express = require('express');
 const { requireAuth, requireRole } = require('../middleware/auth');
 const asyncHandler = require('../utils/asyncHandler');
 const studentController = require('../controllers/studentController');
+const { studentValidators } = require('../validators/studentValidators');
 
 const router = express.Router();
 router.use(requireAuth, requireRole('admin'));
 
 router.get('/', asyncHandler(studentController.list));
 router.get('/:id', asyncHandler(studentController.getOne));
-router.post('/', asyncHandler(studentController.create));
-router.put('/:id', asyncHandler(studentController.update));
+router.post('/', studentValidators, asyncHandler(studentController.create));
+router.put('/:id', studentValidators, asyncHandler(studentController.update));
 router.delete('/:id/permanent', asyncHandler(studentController.hardRemove));
 router.delete('/:id', asyncHandler(studentController.remove));
 
