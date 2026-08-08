46fc7f73c719d36e19fcc4d998b8253a3cfce48c
Marisol Lamas <marilamas02@gmail.com>
2026-08-08 11:11:55 -0300

    feat: agrega validaciones a las rutas de pago, plan y liquidación para mejorar la gestión de datos

--- server/src/routes/planRoutes.js
+++ server/src/routes/planRoutes.js
@@ -1,5 +1,6 @@
 const Plan = require('../models/Plan');
 const createCrudController = require('../controllers/crudFactory');
 const buildCrudRouter = require('../utils/crudRouter');
+const { planValidators } = require('../validators/planValidators');
 
-module.exports = buildCrudRouter(createCrudController(Plan));
+module.exports = buildCrudRouter(createCrudController(Plan), { create: planValidators, update: planValidators });
